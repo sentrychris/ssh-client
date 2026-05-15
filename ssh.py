8f501c13a11d2533598d7382c9bf678b0c8d2860
christopher rowles <c.rowles@sentrybay.com>
2026-05-15 23:00:12 +0100

    Remove deprecated top-level IOLoop

--- ssh.py
+++ ssh.py
@@ -1,22 +1,12 @@
+import asyncio
 import uuid
 import os.path
 
 from tornado.options import define, options, parse_command_line
-from tornado.ioloop import IOLoop
 from app import create_app
 
 # Define base directory
 base_dir = os.path.dirname(__file__)
-
-
-# Create application with specified settings
-app = create_app({
-    'template_path': os.path.join(base_dir, 'public'),
-    'static_path': os.path.join(base_dir, 'public'),
-    'cookie_secret': uuid.uuid1().hex,
-    'xsrf_cookies': False,
-    'debug': True
-})
 
 
 # Define command-line options
@@ -24,7 +14,7 @@ define('address', default='0.0.0.0', help='Listen address for the application')
 define('port', default=4500, help='Listen port for the application', type=int)
 
 
-def run():
+async def run():
     """
     Starts the tornado application server and listens on the specified address and port.
     """
@@ -32,13 +22,19 @@ def run():
     # Parse command line arguments
     parse_command_line()
 
-    print("Port:", options.port)
+    app = create_app({
+        'template_path': os.path.join(base_dir, 'public'),
+        'static_path': os.path.join(base_dir, 'public'),
+        'cookie_secret': uuid.uuid1().hex,
+        'xsrf_cookies': False,
+        'debug': True
+    })
 
     # Start the tornado server
     app.listen(options.port, options.address)
     print("Listening on http://{}:{}".format(options.address, options.port))
-    IOLoop.current().start()
+    await asyncio.Event().wait()
 
 
 if __name__ == '__main__':
-    run()
+    asyncio.run(run())
